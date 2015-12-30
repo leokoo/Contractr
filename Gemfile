fff72c7d0103410feba244e01f7cc7117f8e2cba
Leo Koo <leokoo@gmail.com>
2015-12-30 22:59:04 +0800

    installed linkedin gem

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ gem 'omniauth-facebook'
 gem 'omniauth-github'
 gem 'figaro'
 gem 'toastr-rails'
+gem 'linkedin'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -240,6 +240,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :facebook, ENV['facebook_key'], ENV['facebook_secret'], scope: 'email', info_fields: 'email, name'
+  config.omniauth :github, ENV['github_client_id'], ENV['github_client_secret']
   # config.omniauth :facebook, 'abc', 'abc', scope: 'email', info_fields: 'email, name'
 
   # ==> Warden configuration
