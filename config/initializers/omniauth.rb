2e6d5aad957e1f5ca3d92303ccb781dfdfca4f48
Leo Koo <leokoo@gmail.com>
2015-12-31 00:36:19 +0800

    Reverted back as multiple models omniauth didn't work :(
    https://github.com/plataformatec/devise/wiki/OmniAuth-with-multiple-mode
    ls

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['facebook_key'], ENV['facebook_secret'], scope: 'email', info_fields: 'email, name'
-  provider :github, ENV['github_client_id'], ENV['github_client_secret']
-  on_failure { |env| AuthenticationsController.action(:failure).call(env) }
-end
