2f08fedfd531a89ab1547c03cc055dd72be39ff0
Leo Koo <leokoo@gmail.com>
2015-12-31 12:13:37 +0800

    adding Google auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,21 @@ class User < ActiveRecord::Base
          
   validates :fullname, presence: true, length: {maximum: 50}
 
-  def self.create_with_omniauth(info)
-    create(name: info['name'])
+   def self.from_omniauth(auth)
+    user = User.where(email: auth.info.email).first
+
+    if user
+      return user
+    else
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.fullname = auth.info.name
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.email = auth.info.email
+        user.image = auth.info.image
+        user.password = Devise.friendly_token[0,20]
+      end
+    end
   end
 
 end
