b9d93e08ac582e704ae092721f5e228068046615
Leo Koo <leokoo@gmail.com>
2015-12-31 14:33:35 +0800

    Trying with an alternative github login

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -23,16 +23,9 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 end
 
-	def github
-  @user = User.from_omniauth(request.env["omniauth.auth"])
-  byebug
-  if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication
-      set_flash_message(:notice, :success, :kind => "Github") if is_navigational_format?
-  else
-      session["devise.github_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+  def github
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    sign_in_and_redirect @user
   end
-end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,11 +14,9 @@ class User < ActiveRecord::Base
       return user
     else
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-        user.fullname = auth.info.name
         user.provider = auth.provider
         user.uid = auth.uid
         user.email = auth.info.email
-        user.image = auth.info.image
         user.password = Devise.friendly_token[0,20]
       end
     end
