695ef5c89b79a2c327c0e25be46759e502db697d
Leo Koo <leokoo@gmail.com>
2015-12-31 16:04:31 +0800

    commented out on Google omniauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -241,7 +241,7 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :facebook, ENV['facebook_key'], ENV['facebook_secret'], scope: 'email', info_fields: 'email, name'
   config.omniauth :github, ENV['github_client_id'], ENV['github_client_secret'], scope: 'user'
-  config.omniauth :google_oauth2, ENV['google_client_id'], ENV['google_client_secret']
+  # config.omniauth :google_oauth2, ENV['google_client_id'], ENV['google_client_secret']
   # config.omniauth :facebook, 'abc', 'abc', scope: 'email', info_fields: 'email, name'
 
   # ==> Warden configuration
