4871a02925edcd256bec35b1c544458de2a4041b
Tharan Raj <tharanseriously@gmail.com>
2016-01-02 23:00:00 +0800

    task fields in nested form using cocoon gem. thanks to Ping aka Lord and Saviour

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,7 +1,10 @@
 class Job < ActiveRecord::Base
 	
+	
 	belongs_to :user
 	has_many :bids
+	has_many :tasks
+	accepts_nested_attributes_for :tasks, reject_if: :all_blank, allow_destroy: true
 
 	searchkick
 end
