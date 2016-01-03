5dbaf14fa0ba0331a508a016ff038b1488cab666
Leo Koo <leokoo@gmail.com>
2016-01-03 23:01:58 +0800

    added description, short_description and image_url fields to Job

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160103095311) do
+ActiveRecord::Schema.define(version: 20160103142944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,10 +42,13 @@ ActiveRecord::Schema.define(version: 20160103095311) do
     t.string   "name"
     t.string   "pay_offer"
     t.integer  "user_id"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.integer  "job_status",      default: 0, null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.integer  "job_status",        default: 0, null: false
     t.datetime "expiration_date"
+    t.text     "short_description"
+    t.text     "description"
+    t.string   "image_url"
   end
 
   create_table "users", force: :cascade do |t|
