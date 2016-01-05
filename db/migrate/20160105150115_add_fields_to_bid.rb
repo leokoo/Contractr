7fa8831df57d159d98c759ec54968fcb55900d63
Leo Koo <leokoo@gmail.com>
2016-01-06 00:50:29 +0800

    Added delivery period and uuid to the Bid model

--- db/migrate/20160105150115_add_fields_to_bid.rb
+++ db/migrate/20160105150115_add_fields_to_bid.rb
@@ -1,5 +1,6 @@
 class AddFieldsToBid < ActiveRecord::Migration
   def change
-  	add_column :bids, :bid_status, :integer
+  	add_column :bids, :uuid, :string
+  	add_column :bids, :delivery_period, :integer
   end
 end
