3aba3cf8e2b29ee0be09eae172a2cac526688884
Leo Koo <leokoo@gmail.com>
2016-01-06 18:36:27 +0800

    fixed the job model

--- app/models/job.rb
+++ app/models/job.rb
@@ -16,18 +16,16 @@
 #
 
 class Job < ActiveRecord::Base
-<<<<<<< HEAD
+
   extend FriendlyId
   friendly_id :slug_candidates, use: :slugged
-  
-=======
->>>>>>> cc22f4d8021006001d06b92f081062ae9e43609d
 	belongs_to :user
 	has_many :job_skills
 	serialize :required_skills
 	has_many :bids
-<<<<<<< HEAD
   has_many :rewards
+  has_many :tasks
+  accepts_nested_attributes_for :tasks, reject_if: :all_blank, allow_destroy: true
 
   validates :name, :short_description, :description, :image_url, :expiration_date, presence: true
 
@@ -50,11 +48,6 @@ class Job < ActiveRecord::Base
         [:name, :created_at]
       ]
     end
-=======
-	has_many :tasks
-	accepts_nested_attributes_for :tasks, reject_if: :all_blank, allow_destroy: true
-
-	searchkick
 
 	def has_skill?(skill)
   	return false if required_skills.nil?
@@ -62,11 +55,9 @@ class Job < ActiveRecord::Base
   	end
 
   	def completion_status
-
   		total = self.tasks.count.to_f
   		completed = self.tasks.where("task_status = true").count.to_f
   		completion_percent = ((completed/total)*100).to_f
   		return completion_percent
   	end
->>>>>>> cc22f4d8021006001d06b92f081062ae9e43609d
 end
